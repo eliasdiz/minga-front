23fc4e45667b3aa07ac2a37110209ebaa99ddb99
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-02-27 03:02:47 -0500

    creacion de Register, con envio de peticion post (#3)

---
+++ src/components/Viewregister/Viewregister.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './viewregister.css'
+import FormRegister from '../FormRegister/FormRegister'
+
+export default function Viewregister() {
+    return (
+    <div className='viewregister'>
+        <FormRegister />
+        <img className='imagen-register' src='./images/Rectangle 81.png' />
+    </div>
+    )
+}
